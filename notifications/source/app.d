2b4957a643ec5a83ec9e4dcf91f5912e65c1be5e
Hiroki Noda <kubo39@gmail.com>
2017-12-04 06:50:57 +0900

    Add server information.

--- notifications/source/app.d
+++ notifications/source/app.d
@@ -1,5 +1,19 @@
-import ddbus;
+import ddbus.thin;
 import std.stdio;
+import std.typecons : Tuple;
+
+alias ServerInformation = Tuple!(string, "name", string, "vendor",
+                                 string, "version_", string, "specVersion");
+
+ServerInformation getServerInformation(Connection conn)
+{
+    Message msg = Message("org.freedesktop.Notifications",
+                          "/org/freedesktop/Notifications",
+                          "org.freedesktop.Notifications",
+                          "GetServerInformation");
+    Message reply = conn.sendWithReplyBlocking(msg, 2000);
+    return reply.readTuple!(ServerInformation)();
+}
 
 string[] getCapabilities(Connection conn)
 {
@@ -15,4 +29,10 @@ void main()
 {
     Connection conn = connectToBus();
     conn.getCapabilities().writeln;
+
+    auto serverInformation = conn.getServerInformation();
+    writeln(serverInformation.name);
+    writeln(serverInformation.vendor);
+    writeln(serverInformation.version_);
+    writeln(serverInformation.specVersion);
 }

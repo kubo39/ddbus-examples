c0999177223d9a700b5dc64eba061016cacacb8b
Hiroki Noda <kubo39@gmail.com>
2017-12-06 20:53:00 +0900

    Add example for desktop notification.

--- notifications/source/app.d
+++ notifications/source/app.d
@@ -2,6 +2,20 @@ import ddbus.thin;
 import std.stdio;
 import std.typecons : Tuple;
 
+auto sendNotification(Connection conn)
+{
+    Message msg = Message("org.freedesktop.Notifications",
+                          "/org/freedesktop/Notifications",
+                          "org.freedesktop.Notifications",
+                          "Notify");
+    msg.build("Test ddbus App", 0U, "mail-unread",
+        "Test", "This is a test of DBus binding for D.",
+        ["close", "Close", "open", "Open"],
+        DBusAny(["hoge": variant(DBusAny(0))]), 5000);
+    Message reply = conn.sendWithReplyBlocking(msg, 2000);
+    return reply.read!uint();
+}
+
 auto getServerInformation(Connection conn)
 {
     alias ServerInformation = Tuple!(string, "name", string, "vendor",
@@ -27,6 +41,9 @@ string[] getCapabilities(Connection conn)
 void main()
 {
     Connection conn = connectToBus();
+
+    conn.sendNotification();
+
     conn.getCapabilities().writeln;
 
     auto serverInformation = conn.getServerInformation();
